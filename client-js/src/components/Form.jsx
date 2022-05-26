ec67ece204e12adf0d4ade5813b6f1c8131fcc63
Jennie <m65pjj@gmail.com>
2022-05-27 03:40:03 +0800

    feat: implement getAuthorsQuery api

--- client-js/src/components/Form.jsx
+++ client-js/src/components/Form.jsx
@@ -1,15 +1,20 @@
-import { useState } from 'react'
+import { useState } from "react";
+import { useQuery } from "@apollo/client";
+import { getAuthorsQuery } from "../queries";
 
-const Form = ({className}) => {
-  const [name, setName] = useState('')
-  const [genre, setGenre] = useState('')
-  const [authorId, setAuthorId] = useState('')
+const Form = ({ className }) => {
+  const [name, setName] = useState("");
+  const [genre, setGenre] = useState("");
+  const [authorId, setAuthorId] = useState("");
+
+  const { loading, data } = useQuery(getAuthorsQuery);
 
   const submitForm = (e) => {
-    e.preventDefault()
-    alert(name)
-  }
+    e.preventDefault();
+    alert(name);
+  };
 
+  console.log(data);
   return (
     <form
       className={`${className} w-[600px] p-10 flex flex-col items-end gap-4 bg-gray-50 `}
@@ -20,7 +25,7 @@ const Form = ({className}) => {
         <input
           className="w-60 p-2 rounded-sm focus:outline-stone-400"
           type="text"
-          onChange={e => setName(e.target.value)}
+          onChange={(e) => setName(e.target.value)}
         />
       </div>
       <div className="field">
@@ -28,24 +33,32 @@ const Form = ({className}) => {
         <input
           className="w-60 p-2 rounded-sm focus:outline-stone-400"
           type="text"
-          onChange={e => setGenre(e.target.value)}
+          onChange={(e) => setGenre(e.target.value)}
         />
       </div>
       <div>
         <label>Author：</label>
         <select
           className="w-60 p-2 rounded-sm focus:outline-stone-400"
-          onChange={e => setAuthorId(e.target.value)}
+          onChange={(e) => setAuthorId(e.target.value)}
         >
           <option>Select author</option>
-          <option disabled>loading Authors...</option>
+          {loading ? (
+            <option disabled>loading Authors...</option>
+          ) : (
+            data.authors.map((author) => (
+              <option key={author.id} value={author.id}>
+                {author.name}
+              </option>
+            ))
+          )}
         </select>
       </div>
       <button className="w-10 h-10 text-white bg-stone-600 rounded-full text-2xl hover:bg-stone-800">
         ＋
       </button>
     </form>
-  )
-}
+  );
+};
 
-export default Form
+export default Form;
