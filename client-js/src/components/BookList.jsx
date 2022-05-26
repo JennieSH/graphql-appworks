832d9282bbb7902bbf32c8a7a41ff14160d0c60f
Jennie <m65pjj@gmail.com>
2022-05-27 03:05:36 +0800

    feat: implement getBooksQuery api

--- client-js/src/components/BookList.jsx
+++ client-js/src/components/BookList.jsx
@@ -1,37 +1,39 @@
-
-import BookDetail from './BookDetail'
+import BookDetail from "./BookDetail";
 import { useQuery } from "@apollo/client";
 import { getBooksQuery } from "../queries";
+import { useState } from "react";
 
 const BookList = () => {
   const { loading, error, data } = useQuery(getBooksQuery);
+  const [selectedBookId, setSelectedBookId] = useState(null);
 
   console.log(data);
-
 
   if (error) return <p>Oops! Error!</p>;
   return (
     <section className="grid grid-cols-3 min-h-screen">
       <div className="col-span-2 p-10">
-        <h1 className="font-bold text-center text-3xl font-mono mb-12">Reading List</h1>
-
+        <h1 className="font-bold text-center text-3xl font-mono mb-12">
+          Reading List
+        </h1>
         <div className="flex gap-3">
-        {loading ? (
-          <p className="animate-pulse">Loading ...</p>
-        ) : (
-          data.books.map((book) => (
-            <button
-              key={book.id}
-              className="p-2 border-2 border-neutral-300 rounded-lg bg-neutral-200 hover:outline outline-offset-2"
-            >
-              {book.name}
-            </button>
-          ))
-        )}
+          {loading ? (
+            <p className="animate-pulse">Loading ...</p>
+          ) : (
+            data.books.map((book) => (
+              <button
+                key={book.id}
+                className="p-2 border-2 border-neutral-300 rounded-lg bg-neutral-200 hover:outline outline-offset-2"
+                onClick={() => setSelectedBookId(book.id)}
+              >
+                {book.name}
+              </button>
+            ))
+          )}
         </div>
       </div>
 
-      <BookDetail />
+      <BookDetail bookId={selectedBookId} />
     </section>
   );
 };
