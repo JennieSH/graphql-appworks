a9f2b6e5f9862349fdc4feea4dcf472034c1f385
jennie.shao <m65pjj@gmail.com>
2022-05-27 23:06:56 +0800

    fix: remove unused code

--- client-js/src/components/BookList.jsx
+++ client-js/src/components/BookList.jsx
@@ -1,33 +1,35 @@
-
-import BookDetail from './BookDetail'
-import { useQuery } from "@apollo/client";
-import { getBooksQuery } from "../queries";
+import BookDetail from "./BookDetail";
 
 const BookList = () => {
-  const { loading, error, data } = useQuery(getBooksQuery);
+  const data = {
+    books: [
+      {
+        id: "1",
+        name: "GraphQL",
+      },
+      {
+        id: "2",
+        name: "React",
+      },
+    ],
+  };
 
-  console.log(data);
-
-
-  if (error) return <p>Oops! Error!</p>;
   return (
     <section className="grid grid-cols-3 min-h-screen">
       <div className="col-span-2 p-10">
-        <h1 className="font-bold text-center text-3xl font-mono mb-12">Reading List</h1>
+        <h1 className="font-bold text-center text-3xl font-mono mb-12">
+          Reading List
+        </h1>
 
-        <div className="flex gap-3">
-        {loading ? (
-          <p className="animate-pulse">Loading ...</p>
-        ) : (
-          data.books.map((book) => (
+        <div className="flex flex-wrap gap-3">
+          {data.books.map((book) => (
             <button
               key={book.id}
               className="p-2 border-2 border-neutral-300 rounded-lg bg-neutral-200 hover:outline outline-offset-2"
             >
               {book.name}
             </button>
-          ))
-        )}
+          ))}
         </div>
       </div>
 
