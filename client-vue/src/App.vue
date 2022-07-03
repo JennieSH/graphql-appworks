d1a41b6b1033d6eba7b80c5f0c6f9ca0b50d28ac
jennie.shao <m65pjj@gmail.com>
2022-07-04 01:56:25 +0800

    chore: remove example code of query

--- client-vue/src/App.vue
+++ client-vue/src/App.vue
@@ -1,14 +1,6 @@
 <script setup lang="ts">
-import { watch } from "vue";
 import BookList from "./components/BookList.vue";
 import Form from "./components/Form.vue";
-import { useBooksQuery } from "./generated/graphql";
-
-const { result, loading } = useBooksQuery();
-
-watch(() => {
-  console.log(result.value);
-});
 </script>
 
 <template>
